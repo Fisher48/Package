c82ae807c68307424ea02dcac2907b8a42a7d44b
Fisher48 <rybakov48@yandex.ru>
2024-02-12 22:06:37 +0300

    Added logger and log.info

--- Task.java
+++ Task.java
@@ -1,10 +1,15 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.logging.Logger;
+
 import myDamage.Damage;
+
 public class Task {
+    static Logger log = Logger.getLogger(Task.class.getName());
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
+        log.info("Program started and constructed.");
 
         System.out.println("Киньте кубик:"); // шанс успешной атаки
         int r = rand.nextInt(10)+1;
