2fb1f6b58a4ca921c6de9150b5314088f7822a53
Fisher48 <rybakov48@yandex.ru>
2024-02-12 22:08:25 +0300

    Added log in myDamage & added assert

--- myDamage/Damage.java
+++ myDamage/Damage.java
@@ -1,8 +1,14 @@
 package myDamage;
 
+import java.util.logging.Logger;
 import myHealth.Health;
 public class Damage {
+    static Logger log = Logger.getLogger(Damage.class.getName());
     public static int Dam(int x) {
+        if (x < 0) {
+            log.warning("Damage entered incorrectly");
+        }
+        assert (x > 0) : "Отрицательный урон: " + x;
         if (x <= 1000) {
             return Health.HP(x);
         }
